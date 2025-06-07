a8d2887ed374b6e987992ddaf3e967bdfee65bbb
JulianGerstner <julygerstner@hotmail.com>
2025-06-06 21:09:27 -0300

    Add button for resize

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const container = document.querySelector(".container");
-
-for(i=0;i<256;i++){
-const div = document.createElement("div");
-container.appendChild(div);
-div.addEventListener('mouseenter', ()=>{
+let gridSize = 5;
+function createGrid(size){
+const itemSize= 100 / gridSize;
+container.innerHTML = ''
+for(i=0;i<gridSize*gridSize;i++){
+    
+    const div = document.createElement("div");
+    div.style.width = itemSize + "%";
+    div.style.height = itemSize + "%";
+    container.appendChild(div);
+    div.addEventListener('mouseenter', ()=>{
     div.style.backgroundColor = "black";
 })
 }
+}
+createGrid(gridSize);
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", ()=>{
+    gridSize=parseInt(prompt("Grid size:"));
+    createGrid(gridSize);
+})
 
-//const btn = document.querySelector("#btn");
